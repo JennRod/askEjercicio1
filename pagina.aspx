f613574a457ff7cf4bb4dac5be1cc71aeae3641b
JennRod <97406950+JennRod@users.noreply.github.com>
2022-01-09 13:24:40 -0600

    webaplication2

---
+++ pagina.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="pagina.aspx.cs" Inherits="WebApplication2.pagina" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+        </asp:GridView>
+        <asp:Button ID="Button1" runat="server" Height="74px" OnClick="Button1_Click" Text="Consultar" Width="112px" />
+    </form>
+</body>
+</html>
---
+++ pagina.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class pagina : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Consultar()
+        {
+            using (ServiceReference1.PersonaClient cliente=new PersonaClient())
+            {
+                GridView1.DataSource = cliente.Consultar();
+                GridView1.DataBind();
+
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
